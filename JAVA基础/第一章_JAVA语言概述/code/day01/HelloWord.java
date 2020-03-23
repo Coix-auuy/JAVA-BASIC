6c3d3780741b4fbec4e20d8229b6173c1a04aaab
coixhe <gb_zixianhe@163.com>
2020-03-23 17:21:29 +0800

    2020.3.23

---
+++ JAVA基础/第一章_JAVA语言概述/code/day01/HelloWord.java
@@ -0,0 +1,15 @@
+
+public class HelloWord {
+    // 中文afdaf注释
+    /*
+     * 
+     * 中文dasdas
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello Word!");
+    }
+}
+
+class class1 {
+
+}
